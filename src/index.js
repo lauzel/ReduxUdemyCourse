b7f6c337f99205c72aa87009f0975ecdb0045374
Alexandre PONCET <alexandreponcet.mail@gmail.com>
2016-11-20 18:34:01 +0100

    Add youtube query, refractor with ES6

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-//Youtube API Key
 const API_KEY = "AIzaSyAQRxb_BC8Y9k_SklOI-naacRvYAKRfQVo";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App /> , document.querySelector('.container'));
