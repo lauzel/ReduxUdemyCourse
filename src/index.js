010afed1214185414cd121bf2334a00d0199c9d8
Alexandre PONCET <alexandreponcet.mail@gmail.com>
2016-11-20 17:36:47 +0100

    Add youtube API KEY and lib search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Youtube API Key
+const API_KEY = "AIzaSyAQRxb_BC8Y9k_SklOI-naacRvYAKRfQVo";
+
 const App = () => {
   return <div>Hi !</div>;
 }
