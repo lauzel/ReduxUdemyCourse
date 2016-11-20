7aadb04a660faa390743435dc61aa7c2deeca399
Alexandre PONCET <alexandreponcet.mail@gmail.com>
2016-11-20 17:42:12 +0100

    Add component search bar.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
 //Youtube API Key
 const API_KEY = "AIzaSyAQRxb_BC8Y9k_SklOI-naacRvYAKRfQVo";
 
 const App = () => {
-  return <div>Hi !</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App /> , document.querySelector('.container'));
